5e2e38ef93e29b54c02b2f49faa0fcf18d5521a3
Wide-Pants <ajtwoddl1236@naver.com>
2026-03-14 19:57:52 +0900

    refactor: chat 공개 export, feature barrel 정리

--- src/features/chat/index.ts
+++ src/features/chat/index.ts
@@ -9,6 +9,18 @@ export {
   sendImageMessage,
   sendTextMessage,
 } from "./api";
+export type {
+  ChatRoomDto,
+  ChatRoomInfoDto,
+  ChatRoomListItemDto,
+  ChatRoomUserDto,
+  CreateChatRoomFailureDto,
+  CreateChatRoomResponseDto,
+  CreateChatRoomSuccessDto,
+  MessageDto,
+  MessageListDto,
+  UserLastReadMessageIdDto,
+} from "./api/client/dto.schema";
 export { fetchRoomListApi } from "./api/server";
 export {
   AddChatRoomButton,
@@ -28,25 +40,37 @@ export {
 } from "./components";
 export { INFINITE_SCROLL_DEBOUNCE_MS } from "./constants";
 export {
-  getChatRoomTitle,
-  getChatRoomTitleFromListItem,
-  useChatNotification,
-  useChatRoomFetchOlderPageTrigger,
-  useChatRoomMessageList,
-  useChatRoomSocketMessages,
-  useChatRoomTitle,
-  useChatRoomUserMaps,
+  useCreateChatRoomFromFriendEmails,
   useExitChatRoom,
-  useMaintainScrollOnRoomMessageListChange,
-  useMessageList,
   useOpenPersonalChatNavigation,
   usePendingMessages,
+  type UsePendingMessagesParams,
+} from "./hooks/actions";
+export {
+  useChatRoomFetchOlderPageTrigger,
+  useChatRoomSocketMessages,
+  useMaintainScrollOnRoomMessageListChange,
   useResetRoomUnreadCount,
   useScrollPosition,
   useSyncChatRoomFocusOnRoomId,
-} from "./hooks";
+} from "./hooks/state";
+export {
+  getChatRoomTitle,
+  getChatRoomTitleFromListItem,
+  useChatNotification,
+  useChatRoomMessageList,
+  useChatRoomTitle,
+  useChatRoomUserMaps,
+  useMessageList,
+} from "./hooks/view-model";
 export { chatMutationOptions, chatQueries } from "./queries";
-export { useRoomListSocket,useRoomReadSocket } from "./socket";
-export { useChatRoomStore } from "./store";
+export { useRoomListSocket, useRoomReadSocket } from "./socket";
+export {
+  SelectFriendModal,
+  SelectFriendModalProvider,
+  useChatRoomStore,
+  useSelectFriendModal,
+  useSelectFriendModalStore,
+} from "./store";
 export type { Message } from "./types";
 export { isImageMessage, isTextMessage } from "./types";
