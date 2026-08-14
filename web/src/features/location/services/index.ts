553a053317362b09a555501ab9a19b7e9d7a2e72
Wide-Pants <ajtwoddl1236@naver.com>
2026-08-14 15:31:00 +0900

    feat: 학교/서울역 위치 fallback 해석 추가

--- web/src/features/location/services/index.ts
+++ web/src/features/location/services/index.ts
@@ -1,2 +1,3 @@
 export { getGeolocationUserMessage } from "./get-geolocation-user-message";
+export { resolveFallbackLocation } from "./resolve-fallback-location";
 export { updateLocation } from "./updateLocation";
